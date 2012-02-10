f2a595dbb1f097587bc5409f0121ec119348c294
Eivind Siqveland Larsen <to.eivind@gmail.com>
2012-02-10 13:54:21 +0100

    mikrokontrollere: working 1 button with software delayed exec

--- mikrokontrollere_tdt4258/oving1/oeving1/oeving1.s
+++ mikrokontrollere_tdt4258/oving1/oeving1/oeving1.s
@@ -19,17 +19,19 @@ SR_GM =   16  /* statusregisterflag "GM" er bit 16 */
 _start: /* programutføring vil starte her */
 	
 	lddpc r0, _pio_b /* load address for PIOB into r0 */
+	lddpc r1, _pio_c /* load address for PIOB into r0 */
 
-/*	mov r2, 0b00001111	*/
 	mov r2, 0x00FF 
-	movh r1, 0x00FF 
-	add r2, r1  
-	mov r3, 0b11111111
-	st.w r0[AVR32_PIO_PER], r2 /* enable these io pins */
-	st.w r0[AVR32_PIO_OER], r3 /* enable output on these io pins */
-/* 11111111 00000000 00000000 = 0xFF0000 */
-/*	movh r2, 0x00FF */
-/*	st.w r0[AVR32_PIO_PUER], r2  */
+/*	orh r2, 0x00FF  */
+	st.w r0[AVR32_PIO_PER], r2 /* enable these io pins on pio_b */
+	st.w r1[AVR32_PIO_PER], r2 /* enable these io pins on pio_c */
+
+	mov r3, 0b11111111	   /* pins 0-7 */
+	st.w r0[AVR32_PIO_OER], r3 /* enable output on these io pins on pio_b */
+
+/* enable pullups on pins 16-23 */
+	mov r2, 0x00FF
+	st.w r1[AVR32_PIO_PUER], r2  
 	
         /* eksempel på skriving til minneområdet "eksempelvariabel" */
 /*	lddpc r0, eksempelvariabel_peker  /* last peker til r0 */
@@ -39,35 +41,57 @@ _start: /* programutføring vil starte her */
         /* eksempel på lesing fra minneområdet "eksempelvariabel" */
 /*	lddpc r0, eksempelvariabel_peker  /* last peker til r0 */
 /*	ld.w r1, r0  /* last fra minnet til r1 */
-	mov r8, 0b00001000
-	st.w r0[AVR32_PIO_SODR], r8 /* set output to 1 */
+
+	mov r8, 0b00010000
+	st.w r0[AVR32_PIO_SODR], r8 /* Turn on leds */
+
 /*	mov r9, 0b01110000 */
 loop:   /* evig løkke */
 /*	st.w r0[AVR32_PIO_SODR], r8 /* set output to 1 */
 /*	st.w r0[AVR32_PIO_CODR], r8 /* set output off */ 
-/*	ld.w r7, r0[AVR32_PIO_PDSR]
+	ld.w r7, r1[AVR32_PIO_PDSR]
+	mov r4, 0xFF
+	and r7, r4
 	sub r7, 0b11111110 
-	breq move_led
+	breq move_led 
 /*	st.w r0[AVR32_PIO_SODR], r9 /* set output to 1 */
 /*	st.w r0[AVR32_PIO_CODR], r9 /* set output off */
 	
 /*	lddpc r0[AVR32_PIO_PDSR] */
         rjmp loop
-/*move_led:
+move_led:
 	lsl r8, 1
+	mov r5, 0x100
+	sub r5, r8
+	breq set_shifter
+update_leds:
 	mov r2, 0b11111111
 	st.w r0[AVR32_PIO_CODR], r2
 	st.w r0[AVR32_PIO_SODR], r8
-	rjmp mindless_loop
+	mov r6, 200000
+	rjmp intr_sleep_start
+	rjmp loop
 
-mindless_loop:
-	rjmp mindless_loop*/
+set_shifter:
+	mov r8, 1
+	rjmp update_leds
+
+intr_sleep_start:
+	sub r6, 1
+	cp.w r6, 0
+	breq loop
+	rjmp intr_sleep_start
+
+intr_sleep_end:
+	
+
 /* peker til AVR32_PIOB variabelen, brukes av lddpc-instruksjonen */
 _turn_on:
 	.int ENABLE_PINS
 _pio_b:
 	.int AVR32_PIOB
-
+_pio_c:
+	.int AVR32_PIOC
 /*****************************************************************************/
 /* data-segment */
 /* alle dataområder som skal kunne skrives til må plasseres her */
