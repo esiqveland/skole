57a40fd3288160519b6b3b0a0b76aaeca230d564
Eivind Siqveland Larsen <to.eivind@gmail.com>
2012-03-07 15:24:06 +0100

    compilers: oving4 part2: more code

--- compilers_tdt4205/oving4/code/ps4_skeleton/src/symtab.c
+++ compilers_tdt4205/oving4/code/ps4_skeleton/src/symtab.c
@@ -14,15 +14,16 @@ symtab_init ( void )
 {
 	/* initialize scopes */
 	scopes = malloc(sizeof(hash_t*)*scopes_size);
-	for(int i = 0; i < scopes_size; i++) {
-		scopes[i] = ght_create(10); /* (N is # of hash buckets), don't know what this is supposed to be */
-	}
+//	for(int i = 0; i < scopes_size; i++) {
+//		scopes[i] = ght_create(10); /* (N is # of hash buckets), 
+//									 * don't know what this is supposed to be */
+//	}
 
 	/* init values */
 	values = malloc(sizeof(symbol_t*)*values_size);
-	for(int i = 0; i < values_size; i++) {
-		values[i] = malloc(sizeof(symbol_t));
-	}
+//	for(int i = 0; i < values_size; i++) {
+//		values[i] = malloc(sizeof(symbol_t));
+//	}
 
 	/* init strings */
 	strings = malloc(sizeof(char*)*strings_size);
@@ -32,37 +33,74 @@ symtab_init ( void )
 void
 symtab_finalize ( void )
 {
+
 }
 
 
 int32_t
 strings_add ( char *str )
 {
-    return 0;
+	strings_index++;
+	if(strings_index == strings_size) {
+		realloc(strings, sizeof(char*)*strings_size*2);
+		strings_size = strings_size*2;
+	}
+	strings[strings_index] = str;
+	
+    return strings_index;
 }
 
 
 void
 strings_output ( FILE *stream )
 {
+	fprintf( stream, "\n.data\n.INTEGER:\t.string \"%d \"\n" );
+	for(int i = 0; i < strings_size; i++) {
+		fprintf( stream, ".STRING%d:\t.string \"%s\"\n", i, strings[i] );
+	}
+	fprintf( stream, "\n.globl main\n" );
 }
 
 
 void
 scope_add ( void )
 {
+	scopes_index++;
+	if(scopes_index == scopes_size) {
+		scopes = realloc(scopes, sizeof(hash_t*)*2*scopes_size);
+		scopes_size = scopes_size*2;
+	}
+	scopes[scopes_index] = ght_create(10);
 }
 
 
 void
 scope_remove ( void )
 {
+	if(scopes_index < 0)
+		return;
+	// remove uppermost scope
+	hash_t* p_ht = scopes[scopes_index];
+/*	
+	ght_iterator_t iterator;
+	void *p_key;   
+	void *p_e;
+
+	for(p = ght_first(p_ht, &iterator); p; p = ght_next(p, &iterator)) {      
+		free(p);
+	}
+
+	ght_finalize(p_ht);
+	*/
+	free( scopes[scopes_index] );
+	scopes_index--;
 }
 
 
 void
 symbol_insert ( char *key, symbol_t *value )
 {
+
 #ifdef DUMP_SYMTAB
 fprintf ( stderr, "Inserting (%s,%d)\n", key, value->stack_offset );
 #endif
