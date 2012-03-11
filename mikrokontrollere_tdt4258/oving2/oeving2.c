599e254a03e2ef0ac47624e86bb2a572cd3f3546
Eivind Siqveland Larsen <to.eivind@gmail.com>
2012-03-11 18:15:49 +0100

    mikro: oving2: code...

--- mikrokontrollere_tdt4258/oving2/oeving2.c
+++ mikrokontrollere_tdt4258/oving2/oeving2.c
@@ -12,10 +12,10 @@ volatile avr32_dac_t *dac = &AVR32_DAC;
 volatile avr32_sm_t *sm = &AVR32_SM;
 
 int main (int argc, char *argv[]) {
-  initHardware();
+    initHardware();
 
-  while(1);
-  return 0;
+    while(1);
+    return 0;
 }
 
 /* funksjon for å initialisere maskinvaren, må utvides */
@@ -32,12 +32,11 @@ void initIntc(void) {
 }
 
 void initButtons(void) {
-  	register_interrupt( button_isr, AVR32_PIOB_IRQ/32, AVR32_PIOB_IRQ % 32, BUTTONS_INT_LEVEL );
 	/* enable interrupts and pullups: */
 	piob->per = 0xff;
 	piob->puer = 0xff;
 	piob->ier = 0xff;
-
+    register_interrupt( button_isr, AVR32_PIOB_IRQ/32, AVR32_PIOB_IRQ % 32, BUTTONS_INT_LEVEL );
 }
 
 void initLeds(void) {
@@ -48,35 +47,40 @@ void initLeds(void) {
 }
 
 void initAudio(void) {
-  	register_interrupt( abdac_isr, AVR32_ABDAC_IRQ/32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL );
     /* dac is on pins 20+21 on piob
-     *  disable driving (output) on these pins for the io controller */
-    //piob->pdr(0x18000);
-    piob->pdr.p20 = 1;
-    piob->pdr.p21 = 1;
-    piob->asr.p20 = 1;
-    piob->asr.p21 = 1;
+     * disable driving (output) on these pins for the io controller */
+    //piob->pdr = 0x18000;
+    piob->PDR.p20 = 1;
+    piob->PDR.p21 = 1;
+    piob->ASR.p20 = 1;
+    piob->ASR.p21 = 1;
 
     /* power management and clock interrupts
      * clock #6 is connected to the abdac, and must be enabled */
-    sm->pm_gctrl[6] = data;
+    sm->pm_gcctrl[6] = 0x05;
 
     /* enable dac */
     dac->cr.en = 1;
     /* enabled interrupts on dac */
     dac->ier.tx_ready = 1;
+    register_interrupt( abdac_isr, AVR32_ABDAC_IRQ/32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL );
 }
 
-/* does this need debouncing? */
 __int_handler* button_isr(void) 
 {
+    /* debounce */
+    for( volatile int debounce = 1000; debounce > 0; debounce--) { }
+
 	/* read interrupt mask */
 	int irupt = piob->isr;   /* interrupt status register */
 	irupt = irupt*0xff; 	 /* clear unwanted bits  */
+    //irupt = irupt & 0xff;
 	
 	int leds = pioc->pdsr;   /* read LED status */
 	leds = leds*0xff;
 	//leds = leds & 0xff;	 /* can we do bitwise AND? */
+
+    pioc->codr = 0xff; 	 /* turn off all leds */
 
 	if( irupt == 0x01 ) { 	 /* shift LED to the right */
 		leds = leds >> 1;
@@ -89,19 +93,24 @@ __int_handler* button_isr(void)
 			leds = 1;
 		pioc->sodr = leds;
 	} else {				 /* set led to the button pressed */
-		pioc->codr = 0xff; 	 /* turn off all leds */
 		pioc->sodr = irupt;  /* hopefully turn on led for button that was pressed */
 	}
+    /* clear interrupts */
+    int temp = piob->isr;
 	return 0;
 }
 
-void abdac_isr(void) 
+__int_handler* abdac_isr(void)
 {
     /* at interrupt, write a new sample */
-    //dac->sdr.channel0 = (short)rand();
-    //dac->sdr.channel1 = (short)rand();
-    //dac->sdr.channel0 = (short)channeldata;
-    //dac->sdr.channel1 = (short)channeldata;
+    dac->SDR.channel0 = (short)rand();
+    dac->SDR.channel1 = (short)rand();
+    //dac->SDR.channel0 = (short)channeldata;
+    //dac->SDR.channel1 = (short)channeldata;
+
+    /* clear interrupts */
+    int temp = dac->isr;
+    return 0;
 }
 
 /* vim: set ts=4 sw=4: */
