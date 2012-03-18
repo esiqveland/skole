0f67aa51245f5b75e18161848f5d38a2cdfd737d
Eivind Siqveland Larsen <to.eivind@gmail.com>
2012-03-18 13:12:34 +0100

    micro: oving2: updates

--- mikrokontrollere_tdt4258/oving2/oeving2.c
+++ mikrokontrollere_tdt4258/oving2/oeving2.c
@@ -34,7 +34,7 @@ Song* bling;
 int main (int argc, char *argv[]) {
     storeSongs();
     initHardware();
-    //playSong( test );
+
     while(1);
     return 0;
 }
@@ -49,6 +49,13 @@ void playSong( Song* s ) {
 	samples = MyTones[note->tone];
     notesize = note->size;
     done = note->duration;
+
+    // set up abdac clock
+    pm->gcctrl[6] = 0x04;
+    /* enabled interrupts on dac */
+    dac->IER.tx_ready = 1;
+    /* enable dac */
+    dac->CR.en = 1;
 }
 
 /* funksjon for å initialisere maskinvaren, må utvides */
@@ -89,18 +96,9 @@ void initAudio(void) {
     piob->ASR.p20 = 1;
     piob->ASR.p21 = 1;
 
-    /* power management and clock interrupts
-     * clock #6 is connected to the abdac, and must be enabled */
-/*    pm->gcctrl[6] = 0x04;
-
-    // enable dac
-    dac->CR.en = 1;
-    // enabled interrupts on dac
-    dac->IER.tx_ready = 1;*/
     register_interrupt( abdac_isr, AVR32_ABDAC_IRQ/32, AVR32_ABDAC_IRQ % 32, ABDAC_INT_LEVEL );
 }
 
-//__int_handler button_isr(void) 
 void button_isr(void) 
 {
     /* debounce */
@@ -109,16 +107,16 @@ void button_isr(void)
 
 	/* read interrupt mask */
 	int irupt = piob->isr;   /* interrupt status register */
-	//irupt = irupt*0xff; 	 /* clear unwanted bits  */
+    /* clear unwanted bits  */
     irupt = irupt & 0xff;
-	int btn = piob->pdsr;	 /* what buttons are pressed? */
+
+    int btn = piob->pdsr;	 /* what buttons are pressed? */
 	btn &= irupt;
 	if(btn == 0)		     /* no button pressed, we are releasing the button */
 		return;
 
 	int leds = pioc->pdsr;   /* read LED status */
-	//leds = leds*0xff;
-	leds = leds & 0xff;	 /* can we do bitwise AND? */
+    leds &= 0xff;      /* bitwise AND */
 
     pioc->codr = 0xff; 	 	 /* turn off all leds */
 
@@ -134,64 +132,21 @@ void button_isr(void)
 		pioc->sodr = leds;
 	} else if( irupt == 0x02 ) {
  	    playSong(test);
-        // set up abdac clock
-        pm->gcctrl[6] = 0x04;
-        /* enabled interrupts on dac */
-        dac->IER.tx_ready = 1;
-        /* enable dac */
-        dac->CR.en = 1;
-
-		pioc->sodr = irupt;  /* hopefully turn on led for button that was pressed */
+        pioc->sodr = irupt;
 	} else if( irupt == 0x04 ) {
         playSong(test2);
-        // set up abdac clock
-        pm->gcctrl[6] = 0x04;
-        /* enabled interrupts on dac */
-        dac->IER.tx_ready = 1;
-        /* enable dac */
-        dac->CR.en = 1;
-
 		pioc->sodr = irupt;  
 	} else if( irupt == 0x08 ) {
 	    playSong(hp);
-        // set up abdac clock
-        pm->gcctrl[6] = 0x04;
-        /* enabled interrupts on dac */
-        dac->IER.tx_ready = 1;
-        /* enable dac */
-        dac->CR.en = 1;
-
 		pioc->sodr = irupt;  
 	} else if( irupt == 0x10 ) {
 		playSong(bling);
-        // set up abdac clock
-        pm->gcctrl[6] = 0x04;
-        /* enabled interrupts on dac */
-        dac->IER.tx_ready = 1;
-        /* enable dac */
-        dac->CR.en = 1;
-
 		pioc->sodr = irupt;  
 	} else if( irupt == 0x20 ) {
 		playSong(shot);
-        // set up abdac clock
-        pm->gcctrl[6] = 0x04;
-        /* enabled interrupts on dac */
-        dac->IER.tx_ready = 1;
-        /* enable dac */
-        dac->CR.en = 1;
-
 		pioc->sodr = irupt;  
 	} else if( irupt == 0x40 ) {
 		playSong(win);
-        // set up abdac clock
-        pm->gcctrl[6] = 0x04;
-        /* enabled interrupts on dac */
-        dac->IER.tx_ready = 1;
-        /* enable dac */
-        dac->CR.en = 1;
-
-
 		pioc->sodr = irupt;  
 	} else if( irupt == 0x80 ) {
 		pioc->sodr = irupt;  
@@ -203,14 +158,13 @@ void button_isr(void)
 	return;
 }
 
-//__int_handler abdac_isr(void)
 void abdac_isr(void)
 {
     /* at interrupt, write a new sample */
     if(duration >= done) { /* note is done playing, go to next note */
         songindex++;
         if(songindex >= songsize) { /* song is done playing
-                                     * maybe turn off clock here? */
+                                     * turn off dac here */
     		dac->SDR.channel0 = 0;
 		    dac->SDR.channel1 = 0;
             dac->CR.en = 0;
@@ -218,7 +172,8 @@ void abdac_isr(void)
             return;
         }
         // song not done, play next note
-        *note = song->notes[songindex];//MyTones[songindex];
+        *note = song->notes[songindex];
+        // set the new sample to play
 		samples = MyTones[note->tone];
         duration = 0;
         done = note->duration;
@@ -228,11 +183,10 @@ void abdac_isr(void)
     }
     if(notecounter == note->size)
         notecounter = 0;
-    dac->SDR.channel0 = samples[notecounter];//note->tone[notecounter];
-    dac->SDR.channel1 = samples[notecounter];//note->tone[notecounter];
+    dac->SDR.channel0 = samples[notecounter];
+    dac->SDR.channel1 = samples[notecounter];
     duration++;
     notecounter++;
-
 
     /* clear interrupts */
     volatile int temp = dac->isr;
@@ -251,7 +205,8 @@ void storeSongs( void )
 	MyTones[D4] = Dtone;
 	MyTones[E4] = Etone;
 	MyTones[F4] = Ftone;
-	MyTones[G4] = Gtone;
+    MyTones[Fiss4] = Fisstone;
+    MyTones[G4] = Gtone;
 	MyTones[Giss4] = Gisstone;
 	MyTones[A4] = Atone;
 	MyTones[B4] = B4tone;
@@ -260,168 +215,17 @@ void storeSongs( void )
 	MyTones[Ciss5] = Cisshoy;
 	MyTones[D5] = Dhoy;
 
-	MyTones[Fiss4] = Fisstone;
-
-
-	test2 =  malloc(sizeof(Song));
-	Note* temp1 = malloc(sizeof(Note));
-	temp1->duration = HALF*15;
-	temp1->size = CtoneSize;
-	temp1->tone = C4;
-	test2->length=1;
-	test2->notes = temp1;
-
 
 	test = malloc(sizeof(Song));
-	//Note* asdf = malloc(sizeof(Note)*12);
-	Note asdf[] =
-		{
-			{ C4, C4SIZE, HALF },
-			{ D4, D4SIZE, HALF },
-			{ E4, E4SIZE, HALF },
-			{ C4, C4SIZE, HALF },
-			{ C4, C4SIZE, HALF },
-			{ D4, D4SIZE, HALF },
-			{ E4, E4SIZE, HALF },
-			{ C4, C4SIZE, HALF },
-			{ D4, D4SIZE, HALF },
-			{ D4, D4SIZE, HALF },
-			{ D4, D4SIZE, HALF },
-			{ D4, D4SIZE, HALF },
-			{ C4, C4SIZE, HALF*2 },
-			{ C4, C4SIZE, HALF*2 }
-		};
-	Note* fdsa = malloc(sizeof(Note)*14);
-	int i;
-	for(i = 0; i < 14; i++) {
-		fdsa[i].duration = asdf[i].duration;
-		fdsa[i].size = asdf[i].size;
-		fdsa[i].tone = asdf[i].tone;
-	}
-
 	test->length = 14;
-	test->notes = fdsa;
+    test->notes = testnotes;
 
 	hp = malloc(sizeof(Song));
-	Note* hpnotes = malloc(sizeof(Note)*60);
 	hp->length=60;
-	Note temp3[] =
-	{
-			{ H3, HlowSize, HPSPEED*2 },
+    hp->notes = hptemp;
 
-			{ E4, E4SIZE, HPSPEED*3 },
-			{ G4, G4SIZE, HPSPEED },
-			{ Fiss4, Fiss4SIZE, HPSPEED*2 },
-
-			{ E4, E4SIZE, HPSPEED*4 },
-			{ H4, H4SIZE, HPSPEED*2 },
-
-			{ A4, A4SIZE, HPSPEED*6 },
-
-			{ Fiss4, FissSize, HPSPEED*6 },
-
-			{ E4, E4SIZE, HPSPEED*3 },
-			{ G4, G4SIZE, HPSPEED },
-			{ Fiss4, FissSize, HPSPEED*2 },
-
-			{ D4, D4SIZE, HPSPEED*4 },
-			//{ Fiss4, Fiss4SIZE, HPSPEED*2 },
-			{ F4, F4SIZE, HPSPEED*2 },
-
-			{ H3, HlowSize, HPSPEED*10 },
-			{ H3, HlowSize, HPSPEED*2 },
-
-			{ E4, E4SIZE, HPSPEED*3 },
-			{ G4, G4SIZE, HPSPEED },
-			{ Fiss4, Fiss4SIZE, HPSPEED*2 }, /* 18 */
-
-			{ E4, E4SIZE, HPSPEED*4 },
-			{ H4, H4SIZE, HPSPEED*2 },
-
-			{ D5, D5SIZE, HPSPEED*4 },			/* 79 */
-			{ Ciss5, Ciss5SIZE, HPSPEED*2 },   /* 84 */
-
-			{ C5, C5SIZE, HPSPEED*4 },         /* 89 */
-			{ Giss4, Giss4SIZE, HPSPEED*2 },    // giss egentlig
-
-			{ C5, C5SIZE, HPSPEED*3 },
-			{ H4, H4SIZE, HPSPEED },
-			{ B4, B4SIZE, HPSPEED*2 },    // egentlig b for h (b for b?) // 27
-
-			{ B3, B3SIZE, HPSPEED*4 },  // egentlig b for lav h // 28
-			{ G4, G4SIZE, HPSPEED*2 },
-
-			{ E4, E4SIZE, HPSPEED*10 },
-			{ G4, G4SIZE, HPSPEED*2 },
-
-			{ H4, H4SIZE, HPSPEED*4 },
-			{ G4, G4SIZE, HPSPEED*2 },
-
-			{ H4, H4SIZE, HPSPEED*4 },
-			{ G4, G4SIZE, HPSPEED*2 },
-
-			{ C5, ChoySize, HPSPEED*4 },
-			{ H4, H4SIZE, HPSPEED*2 },  /* 37 */
-
-			{ B4, B4SIZE, HPSPEED*4 },    // egentlig b for h (b for b?)
-			{ Fiss4, FissSize, HPSPEED*2 },
-
-			{ G4, G4SIZE, HPSPEED*3 },
-			{ A4, A4SIZE, HPSPEED },
-			{ B4, B4SIZE, HPSPEED*2 },    // egentlig b for h (b for b?)
-
-			{ B3, B3SIZE, HPSPEED*4 },  // egentlig b for lav h
-			{ C4, C4SIZE, HPSPEED*2 },
-
-			{ H3, HlowSize, HPSPEED*10 },
-			{ G4, G4SIZE, HPSPEED*2 },    // 46
-
-			{ H4, H4SIZE, HPSPEED*4 },
-			{ G4, G4SIZE, HPSPEED*2 },
-
-			{ H4, H4SIZE, HPSPEED*4 },
-			{ G4, G4SIZE, HPSPEED*2 },
-
-			{ D5, DhoySize, HPSPEED*4 },
-			{ Ciss5, CisshoySize, HPSPEED*2 },
-
-			{ C5, ChoySize, HPSPEED*4 },
-			{ Giss4, Giss4SIZE, HPSPEED*2 },    // giss egentlig
-
-			{ C5, ChoySize, HPSPEED*3 },
-			{ H4, H4SIZE, HPSPEED },
-			{ B4, B4SIZE, HPSPEED*2 },    // egentlig b for h (b for b?) // 27
-
-			{ B3, B3SIZE, HPSPEED*4 },  // egentlig b for lav h // 28
-			{ G4, G4SIZE, HPSPEED*2 },
-
-			{ E4, E4SIZE, HPSPEED*12 }
-	};
-	for(i = 0; i < 60; i++) {
-		hpnotes[i].duration = temp3[i].duration;
-		hpnotes[i].size = temp3[i].size;
-		hpnotes[i].tone = temp3[i].tone;
-	}
-
-	hp->length = 60;
-	hp->notes = hpnotes;
-
-	Note blingtemp[] =
-    {
-        { C6, C6SIZE, HPSPEED/2 },
-        { C6, C6SIZE, HPSPEED/2 },
-        { F6, F6SIZE, HPSPEED*2 }
-    };
-
-	Note* blingnotes = malloc(sizeof(Note)*3);
 	bling = malloc(sizeof(Song));
 	bling->length=3;
-	for(i = 0; i < 3; i++) {
-		blingnotes[i].duration = blingtemp[i].duration;
-		blingnotes[i].size = blingtemp[i].size;
-		blingnotes[i].tone = blingtemp[i].tone;
-	}
-
 	bling->notes=blingnotes;
 
 	shot = malloc(sizeof(Song));
@@ -431,7 +235,6 @@ void storeSongs( void )
 	win = malloc(sizeof(Song));
 	win->length=4;
 	win->notes = wintemp;
-
 
 }
 
